0f8b34bab7ea32ce36b57321e44c447feec8b969
Olivia <obrown917@gmail.com>
2018-05-19 12:04:08 -0700

    Initial commit

---
+++ ImageBrowser/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  ImageBrowser
+//
+//  Created by Olivia Brown on 5/19/18.
+//  Copyright © 2018 Olivia Brown. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
