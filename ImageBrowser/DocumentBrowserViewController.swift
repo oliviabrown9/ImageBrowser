622dddadc73d667284cce3b132b9442e8bd57366
Olivia <obrown917@gmail.com>
2018-05-19 12:21:10 -0700

    Storyboard

--- ImageBrowser/DocumentBrowserViewController.swift
+++ ImageBrowser/DocumentBrowserViewController.swift
@@ -64,11 +64,11 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
     
     func presentDocument(at documentURL: URL) {
         
-        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        let documentViewController = storyBoard.instantiateViewController(withIdentifier: "DocumentViewController") as! DocumentViewController
-        documentViewController.document = Document(fileURL: documentURL)
-        
-        present(documentViewController, animated: true, completion: nil)
+//        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+//        let documentViewController = storyBoard.instantiateViewController(withIdentifier: "DocumentViewController") as! DocumentViewController
+//        documentViewController.document = Document(fileURL: documentURL)
+//
+//        present(documentViewController, animated: true, completion: nil)
     }
 }
 
